6699b2492a7d6d639a45c3fbffda27f51fa680d9
Guilherme <contato.gspm@gmail.com>
2025-09-04 18:05:54 -0300

    Inserido as funções de adicionar e sorter nome

--- Chaallenge Amigo Secreto/app.js
+++ Chaallenge Amigo Secreto/app.js
@@ -1,1 +1,35 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+listaAmigos = []
+nomeDoAmigoSecreto = sortearAmigo()
+
+function adicionarAmigo(){
+    let amigo = document.querySelector("input").value;
+
+    if (amigo == ''){
+        alert ("Por favor insira um nome válido");
+    } else {
+        listaAmigos.push(amigo)
+        console.log(listaAmigos)
+        exibirTextoNatela("listaAmigos",listaAmigos)
+        limpaNome()
+        return adicionarAmigo
+    }
+}
+
+function limpaNome(){
+    amigo = document.querySelector("input");
+    amigo.value = ''
+}
+
+function sortearAmigo(){
+    let indiceAleatorio = Math.floor(Math.random() * listaAmigos.length);
+    nomeSorteado = listaAmigos[indiceAleatorio]
+    console.log (`O nome sorteado foi ${nomeSorteado}`)
+    return nomeSorteado
+}
+
+function exibirTextoNatela(){
+    let campo = document.querySelector(tag, texto);
+    campo.innerHTML = texto;
+}
+
